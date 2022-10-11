4fb392e4eeebefb15fe457c992c0fda1addd4ea2
Coralie <immacora@gmail.com>
2022-10-11 14:47:45 +0200

    Création des constantes du menu et du main, suppression du procédural

--- functions.py
+++
@@ -1,9 +0,0 @@
-"""Gestion des erreurs"""
-
-def to_integer(answer_str):
-    """Renvoie None si la valeur n'est pas un entier"""
-    try:
-        value = int(answer_str)
-        return value
-    except ValueError:
-        print ("ERREUR: Vous devez rentrer un chiffre")
